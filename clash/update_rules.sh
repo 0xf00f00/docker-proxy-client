439122efa38cc0956a9c80fcd9c56e55fb7cbbe3
0xf00f00 <123018047+0xf00f00@users.noreply.github.com>
2024-10-22 11:44:01 -0700

    Add clash rules

---
+++ clash/update_rules.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download the latest rules
+wget -O ./rules/clash_rules_ir.yaml https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/clash_rules_other.yaml
+wget -O ./rules/clash_rules_ads.yaml https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/clash_rules_ads.yaml
